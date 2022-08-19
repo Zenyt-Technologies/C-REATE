94b5072cf93f3473d19b28ec163f2e48457f5c1c
PetrTech <55279432+PetrTech@users.noreply.github.com>
2022-08-19 15:35:54 +0200

    Add project files.

---
+++ project-create/Program.cs
@@ -0,0 +1,26 @@
+﻿using Raylib_cs;
+using rlImGui_cs;
+using ImGuiNET;
+
+namespace project_create
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Raylib.InitWindow(1280, 720, "C-REATE");
+
+            rlImGui.
+
+            while (!Raylib.WindowShouldClose())
+            {
+                Raylib.ClearBackground(Color.WHITE);
+                
+                Raylib.BeginDrawing();
+                Raylib.EndDrawing();
+            }
+
+            Raylib.CloseWindow();
+        }
+    }
+}
